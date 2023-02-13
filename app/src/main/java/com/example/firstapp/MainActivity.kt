514769c689c40bb8f46e56a852a0160fe100a694
CodyAdam <cody.ad4@gmail.com>
2023-02-13 12:43:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,285 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, heightDp = 1720)
+fun App() {
+    MaterialTheme {
+        Box(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState(), enabled = true)
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                modifier = Modifier
+                    .padding(30.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Part1()
+                Divider()
+                Part2()
+                Divider()
+                Part3()
+                Divider()
+                Part3()
+                Divider()
+                Part5()
+                Divider()
+                Part6()
+                Divider()
+                Part7()
+            }
+        }
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun Part1() {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text("Hello world!")
+        Button(onClick = {}) {
+            Text("Button !")
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun Part2() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text("Hello world!")
+        Button(onClick = {}) {
+            Text("Button !")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview(showBackground = true)
+fun Part3() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Text field") },
+                modifier = Modifier.weight(2f)
+            )
+            // button min width is 48dp
+            Button(onClick = {}, modifier = Modifier.weight(1f)) {
+                Text("Button !")
+            }
+        }
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun Part5() {
+    //Créer une interface avec une list View qui affichera le contenu d’une arraylist que
+    //vous aurez rempli auparavant avec le nom des 4 départements Breton
+    val names by remember {
+        mutableStateOf(
+            listOf(
+                "Côtes-d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan"
+            )
+        )
+    }
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Text(
+            text = "Breton's departments :",
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+        )
+        names.forEach { name ->
+            ListItem(headlineText = {
+                Text(name)
+            }, modifier = Modifier.fillMaxWidth(), leadingContent = {
+                Icon(
+                    Icons.Filled.Favorite,
+                    contentDescription = "Localized description",
+                )
+            },
+                shadowElevation = 4.dp
+            )
+        }
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun Part6() {
+    //Créer une interface avec une list View qui affichera le contenu d’une arraylist que
+    //vous aurez rempli auparavant avec le nom des 4 départements Breton
+    val names by remember {
+        mutableStateOf(
+            listOf(
+                "Côtes-d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan"
+            )
+        )
+    }
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Text(
+            text = "Departments :",
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+        )
+        names.forEach { name ->
+            ListItem(headlineText = {
+                Text("$name - Bretagne")
+            }, modifier = Modifier.fillMaxWidth(), leadingContent = {
+                Icon(
+                    Icons.Filled.Favorite,
+                    contentDescription = "Localized description",
+                )
+            },
+                shadowElevation = 4.dp
+            )
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview(showBackground = true)
+fun Part7() {
+    //Créer une interface avec une list View qui affichera le contenu d’une arraylist que
+    //vous aurez rempli auparavant avec le nom des 4 départements Breton
+    val deps by remember {
+        mutableStateOf(
+            mapOf(
+                "Côtes-d'Armor" to "Côtes-d'Armor (Breton: Kost-ha-Daoul) is a department of France, located in the region of Brittany. It is named for the Côtes-d'Armor peninsula. It is part of the Bretagne historique, or historical Brittany, and is surrounded by the departments of Finistère, Ille-et-Vilaine, and Morbihan.",
+                "Finistère" to "Finistère (Breton: Finistere) is a department of France, located in the region of Brittany. It is named for the Finistère peninsula. It is part of the Bretagne historique, or historical Brittany, and is surrounded by the departments of Côtes-d'Armor, Ille-et-Vilaine, and Morbihan.",
+                "Ille-et-Vilaine" to "Ille-et-Vilaine (Breton: Ille-a-Vilhena) is a department of France, located in the region of Brittany. It is named for the Ille River and the Rance estuary. It is part of the Bretagne historique, or historical Brittany, and is surrounded by the departments of Côtes-d'Armor, Finistère, and Morbihan.",
+                "Morbihan" to "Morbihan (Breton: Morbihan) is a department of France, located in the region of Brittany. It is named for the Morbihan estuary. It is part of the Bretagne historique, or historical Brittany, and is surrounded by the departments of Côtes-d'Armor, Finistère, and Ille-et-Vilaine."
+            )
+        )
+    }
+    var openModal by remember { mutableStateOf(false) }
+    var selected by remember { mutableStateOf("") }
+
+
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Text(
+            text = "Departments informations :",
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+        )
+        deps.forEach { dep ->
+            ListItem(
+                headlineText = {
+                    Text("${dep.key} - Bretagne")
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { selected = dep.key; openModal = true },
+                leadingContent = {
+                    Icon(
+                        Icons.Filled.Info,
+                        contentDescription = "Localized description",
+                    )
+                },
+                trailingContent = {
+                    Icon(
+                        Icons.Filled.KeyboardArrowRight,
+                        contentDescription = "Localized description",
+                    )
+                },
+                shadowElevation = 4.dp
+            )
+        }
+    }
+
+    if (openModal) {
+        ModalBottomSheet(
+            onDismissRequest = { openModal = false },
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Text(
+                    text = "Informations about $selected",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(deps[selected] ?: "", modifier = Modifier.padding(bottom = 60.dp))
+            }
+        }
+    }
+}
+
