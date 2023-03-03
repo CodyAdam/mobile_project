66f156cfb253f89be70f19ab6618ba580e2ecbdd
CodyAdam <cody.ad4@gmail.com>
2023-03-03 17:24:30 +0100

    feat: finish tp4

--- app/src/main/java/com/example/firstapp/ActivityTp4Compose.kt
+++ app/src/main/java/com/example/firstapp/ActivityTp4Compose.kt
@@ -2,6 +2,10 @@ package com.example.firstapp
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.VelocityTracker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -11,19 +15,62 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.unit.dp
 
 class ActivityTp4Compose : ComponentActivity() {
+
+    private var mVelocityTracker: VelocityTracker? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            App(LocalContext.current)
+            App()
         }
     }
+
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        when (event.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                // Reset the velocity tracker back to its initial state.
+                mVelocityTracker?.clear()
+                // If necessary retrieve a new VelocityTracker object to watch the
+                // velocity of a motion.
+                mVelocityTracker = mVelocityTracker ?: VelocityTracker.obtain()
+                // Add a user's movement to the tracker.
+                mVelocityTracker?.addMovement(event)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mVelocityTracker?.apply {
+                    val pointerId: Int = event.getPointerId(event.actionIndex)
+                    addMovement(event)
+                    // When you want to determine the velocity, call
+                    // computeCurrentVelocity(). Then call getXVelocity()
+                    // and getYVelocity() to retrieve the velocity for each pointer ID.
+                    computeCurrentVelocity(1000)
+                    // Log velocity of pixels per second
+                    // Best practice to use VelocityTrackerCompat where possible.
+                    Log.d("", "X velocity: ${getXVelocity(pointerId)}")
+                    Log.d("", "Y velocity: ${getYVelocity(pointerId)}")
+                }
+            }
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                // Return a VelocityTracker object back to be re-used by others.
+                mVelocityTracker?.recycle()
+                mVelocityTracker = null
+            }
+        }
+        return true
+    }
+
 }
 
 @Composable
-private fun App(context: Context) {
+private fun App() {
     MaterialTheme {
         Column(
             Modifier.padding(20.dp)
